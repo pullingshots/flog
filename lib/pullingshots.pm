2bd3901d29968094ee3ccc8835e04efcb47c50ce
Andrew Baerg <baerg@harrar.baerg.ca>
2010-09-13 00:15:33 -0600

    schooled

--- lib/pullingshots.pm
+++ lib/pullingshots.pm
@@ -1,10 +1,31 @@
 package pullingshots;
 use Dancer ':syntax';
+use Dancer::Plugin::Email
 
 our $VERSION = '0.1';
 
 get '/' => sub {
+    email {
+            to => 'andrew.baerg@gmail.com',
+            subject => 'pullingshots.ca: another request',
+            message => request->base,
+        };
+
+
     template 'index';
+
+};
+
+get '/*' => sub {
+    email {
+            to => 'andrew.baerg@gmail.com',
+            subject => 'pullingshots.ca: another request',
+            message => request->base,
+        };
+
+
+    template 'index';
+
 };
 
 true;
