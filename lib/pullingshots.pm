89d596c95b2a1724e8b29c0b16e48101126e6d69
Andrew Baerg <andrew.baerg@gmail.com>
2010-09-23 00:06:03 -0600

    version 1 of the vimblog

--- lib/pullingshots.pm
+++ lib/pullingshots.pm
@@ -5,62 +5,116 @@ use Dancer::Plugin::Database;
 our $VERSION = '0.1';
 
 get '/' => sub {
-    template 'index';
-};
+  use File::Fu;
+  use DateTime;
 
-get '/foo' => sub {
-  my $sth = database->prepare( 'select * from foo' );
-  $sth->execute;
-  template 'index';
+  my $dir = File::Fu->dir('./public/posts/');
+  my @posts = map { 
+    { 
+      updated => DateTime->from_epoch( epoch => $_->stat->mtime ),
+      title   => $_->basename, 
+      content => join("", $_->read),
+      who     => getpwuid($_->stat->uid), 
+    } if $_->is_file 
+  } $dir->list;
+
+  foreach (@posts) { 
+	$_->{content} =~ s/\n\n/<\/p><p>/g;
+	$_->{content} = "<p>" . $_->{content} . "</p>"; 
+    $_->{slug} = $_->{title};
+    $_->{slug} =~ s/\s/_/g;
+  } 
+
+  template 'index', { posts => \@posts };
 };
 
 get qr{/(rdf|rss|atom).*} => sub {
-use XML::Atom::SimpleFeed;
+  use XML::Atom::SimpleFeed;
+  use File::Fu;
+  use DateTime;
+  use DateTime::Format::Atom;
+
+  my $fa = DateTime::Format::Atom->new();
+
+  my $dir = File::Fu->dir('./public/posts/');
+  my @posts = map { 
+    { 
+      updated => DateTime->from_epoch( epoch => $_->stat->mtime ),
+      title   => $_->basename, 
+      content => join("", $_->read),
+      who     => getpwuid($_->stat->uid), 
+    } if $_->is_file 
+  } $dir->list;
+
+  foreach (@posts) { 
+	$_->{content} =~ s/\n\n/<\/p><p>/g;
+	$_->{content} = "<p>" . $_->{content} . "</p>";
+  }
  
- my $feed = XML::Atom::SimpleFeed->new(
+  my $updated = DateTime->from_epoch( epoch => $dir->stat->mtime );
+  my $feed = XML::Atom::SimpleFeed->new(
      title   => 'pullingshots',
      link    => 'http://pullingshots.ca/',
      link    => { rel => 'self', href => 'http://pullingshots.ca/atom', },
-     updated => '2010-09-17T02:13:02Z',
+     updated => $fa->format_datetime($updated),
      author  => 'Andrew Baerg',
      id      => 'urn:uuid:60a76c80-d399-11d9-b93C-0003939e0af6',
- );
- 
- $feed->add_entry(
-     title     => 'schooled',
-     link      => 'http://pullingshots.ca/schooled',
-     id        => 'urn:uuid:1225c695-cfb8-4ebb-aaaa-80da344efa6b',
-     summary   => q|
-<p>
-September always feels a lot more like a new year than the New Year does.
-</p>
-<p>
-Here's a fresh new pullingshots.ca for the new New Year. It's running
-on a <a href="http://www.disklessworkstations.com/200118.html?id=pX6fEGwb">new machine</a> (an old Dell machine from 2000 had faithfully served up eureca.ca, baerg.ca, and pullingshots.ca for many years),
-in a <a href="http://www.flickr.com/photos/baergaj/4997509575/">new location</a> (previously in my office),
-with a <a href="http://www.ubuntu.com/desktop">new OS</a> (I am embarrassed to admit I had still been using RedHat 7.2),
-and using a <a href="http://perldancer.org">new framework</a> (I had been using a crufty old php based blog).
-That's a lot of newness!
-</p>
-<p>
-<a href="http://www.flickr.com/photos/baergaj/4943726575/" title="BLEEP! CLICK! ZAP! PING! by baergaj, on Flickr"><img src="http://farm5.static.flickr.com/4122/4943726575_da37debaaa.jpg" width="500" height="334" alt="BLEEP! CLICK! ZAP! PING!" /></a>
-</p>
-<p><i>p.s. If you're feeling nostalgic, here's <a href="http://old.pullingshots.ca/">the old pullingshots.ca</a></i></p>
-|,
-     updated   => '2010-09-17T02:30:02Z',
+  );
+
+  foreach (@posts) {
+    my $slug = $_->{title};
+    $slug =~ s/\s/_/g;
+    $feed->add_entry(
+     title     => $_->{title},
+     link      => 'http://pullingshots.ca/' . $slug,
+     id        => 'urn:uuid:' . $slug,
+     summary   => $_->{content},
+     updated   => $fa->format_datetime($_->{updated}),
      category  => 'Miscellaneous',
- );
+    );
+  }
  
   content_type 'application/xhtml+xml';
 
  $feed->as_string;
 };
 
-get qr{.*} => sub {
+get '/:post' => sub {
+  use File::Fu;
+  use Try::Tiny;
+  use DateTime;
 
-    warning 'base: ' . request->base . ' path: ' . request->path . ' client: ' . request->user_agent;
-    template 'index';
+  my $fn = params->{post};
+  $fn =~ s/_/ /g;
 
+  my $error;
+  my $file;
+  try {
+    $file = File::Fu->file('./public/posts/' . $fn);
+    $file->stat->mtime;
+  } catch {
+    status 'not_found';
+    $error = "Nothing to see here.";
+  };
+  return $error if $error;
+
+  my @posts = (
+    { 
+      updated => DateTime->from_epoch( epoch => $file->stat->mtime ),
+      title   => $file->basename, 
+      content => join("", $file->read),
+      who     => getpwuid($file->stat->uid), 
+    } 
+  );
+
+  foreach (@posts) { 
+	$_->{content} =~ s/\n\n/<\/p><p>/g;
+	$_->{content} = "<p>" . $_->{content} . "</p>"; 
+    $_->{slug} = $_->{title};
+    $_->{slug} =~ s/\s/_/g;
+  } 
+
+  template 'index', { posts => \@posts };
 };
 
 true;
