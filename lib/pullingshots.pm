143511511b63e2aa6828f72c72bed34f72c87722
Andrew Baerg <andrew.baerg@gmail.com>
2010-10-12 23:23:15 -0600

    images!

--- lib/pullingshots.pm
+++ lib/pullingshots.pm
@@ -23,16 +23,18 @@ sub posts {
     @files = $dir->list;
   }
 
-  my @posts = map { 
-    { 
+  my @posts;
+  foreach (@files) {
+    next if $_->is_dir;
+    push @posts,  { 
       updated => DateTime->from_epoch( epoch => $_->stat->mtime ),
       title   => $_->basename, 
       content => join("", $_->read),
       who     => getpwuid($_->stat->uid), 
-    } if $_->is_file 
-  } @files;
+    }
+  }
 
-  foreach (@posts) { 
+  foreach (@posts) {
     $_->{slug} = $_->{title};
     $_->{slug} =~ s/\s/_/g;
     $_->{slug} = uri_encode($_->{slug}, true);
@@ -40,6 +42,66 @@ sub posts {
   }
 
   @posts
+}
+
+sub images {
+  use File::Fu;
+  use DateTime;
+  use URI::Encode qw(uri_encode uri_decode);
+
+  my $dir = File::Fu->dir(config->{appdir} . '/public/posts/images/');
+  my @files;
+
+  my $fn = shift;
+  if ($fn) {
+    $fn = uri_decode($fn);
+    $fn =~ s/_/ /g;
+    @files = $dir->find(sub{ /\/\Q$fn\E\..+$/ });
+  }
+  else {
+    @files = $dir->list;
+  }
+
+  my @images;
+  foreach (@files) {
+    next if $_->is_dir;
+    push @images,  { 
+      updated => DateTime->from_epoch( epoch => $_->stat->mtime ),
+      filename   => $_->basename, 
+      who     => getpwuid($_->stat->uid), 
+    }
+  }
+
+  use Image::Thumbnail;
+  my $res = shift || '600';
+  my $thumbdir = File::Fu->dir(config->{appdir} . '/public/images/' . $res . '/');
+  if (!$thumbdir->e) { $thumbdir->create; }
+  foreach (@files) {
+    if ($_->is_file) {
+      my $thumbfn = $_->basename;
+      my @thumb = $thumbdir->find(sub{ /\/\Q$thumbfn\E$/ });
+      if (!@thumb) {
+        my $t = new Image::Thumbnail(
+                size       => $res,
+                create     => 1,
+		module     => "Image::Magick",
+                input      => config->{appdir} . '/public/posts/images/' . $_->basename,
+                outputpath => config->{appdir} . '/public/images/' . $res . '/' . $_->basename,
+        );
+      }
+    }
+  }
+
+  foreach (@images) {
+    $_->{title} = $_->{filename};
+    $_->{title} =~ s/\..+$//;
+    $_->{slug}  = $_->{title};
+    $_->{slug}  =~ s/\s/_/g;
+    $_->{slug}  = uri_encode($_->{slug}, true);
+    $_->{html}  = "<p><a href='/images/posts/" . $_->{filename} . "'><img src='/images/$res/" . $_->{filename} . "' /></a></p>";
+  }
+
+  @images
 }
 
 get '/' => sub {
@@ -62,6 +124,7 @@ get qr{/(rdf|rss|atom).*} => sub {
   use DateTime::Format::Atom;
 
   my @posts = posts;
+  my @images = images;
   my $fa = DateTime::Format::Atom->new();
   my $dir = File::Fu->dir(config->{appdir} . '/public/posts/');
 
@@ -85,10 +148,27 @@ get qr{/(rdf|rss|atom).*} => sub {
      category  => 'Miscellaneous',
     );
   }
+  foreach (@images) {
+    $feed->add_entry(
+     title     => $_->{title},
+     link      => uri_for('/images/' . $_->{slug}),
+     id        => 'urn:uuid:' . $_->{slug},
+     summary   => $_->{html},
+     updated   => $fa->format_datetime($_->{updated}),
+     category  => 'Miscellaneous',
+    );
+  }
  
   content_type 'application/xhtml+xml';
 
  $feed->as_string;
+};
+
+get '/images/' => sub {
+
+  my @images = images('', '640');
+
+  template 'image_index', { images => \@images, res => '640' };
 };
 
 get '/:post' => sub {
@@ -101,6 +181,18 @@ get '/:post' => sub {
   }
 
   template 'index', { posts => \@posts };
+};
+
+get '/images/:image' => sub {
+
+  my @images = images(params->{image}, 1024);
+
+  if (! scalar @images) {
+    status 'not_found';
+    return "<a href=\"/\">Move along.</a> Nothing to see here.";
+  }
+
+  template 'image_index', { images => \@images, res => '1024' };
 };
 
 true;
